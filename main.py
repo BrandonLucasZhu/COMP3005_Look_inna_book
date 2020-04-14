1d41535e8b5ef72f6c363b20b82e383f2206b69d
Brandon Zhu <Brandonlucaszhu@gmail.com>
2020-04-14 12:14:05 -0400

    Got add cart, signup and login to work

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import os
-from models import db, Books, Users, login_manager
+from models import db, Books, Users, login_manager, Published, Publisher, Author, Wrote
 from flask_login import current_user, login_user
 
 
@@ -47,7 +47,10 @@ def store():
     main_dir = os.path.dirname(working_dir)
     
     #Query all books in books relation 
-    bookstock = Books.query.all()
+    bookstock = db.session.query(Books.title,Books.book_id, Books.isbn, Author.firstname, Author.lastname, Publisher.publisher_name,  Books.book_stock, Books.price)\
+    .filter(Books.book_id == Wrote.book_id).filter(Books.isbn == Wrote.isbn).filter(Author.a_id == Wrote.a_id) \
+    .filter(Published.book_id == Books.book_id).filter(Published.isbn == Books.isbn).filter(Publisher.p_id == Published.p_id)
+
 
     #Check is user is logged in
     if session.get('logged_in_user') is None:
@@ -55,6 +58,17 @@ def store():
     else:
         username = session.get('logged_in_user')
 
+        if request.method == 'POST':  
+            if request.form['add_to_cart'] == "Add To Cart":
+                #print(request.form['bookisbn'])
+                #print(request.form['bookid'])
+
+                #store isbn's into the cart
+                temp_cart = session['user_cart']
+                temp_cart.append(request.form['bookisbn'])
+                session['user_cart'] = temp_cart
+                
+    #print(bookstock)
     return render_template('store.html', bookstock=bookstock, username=username)
 
 #render login page
@@ -72,13 +86,15 @@ def login():
             #login validation -> check if database has records
             loginval = Users.query.filter_by(username = request.form['username'], password = request.form['password'])
             
+
+
             if loginval.count() == 0:
                 error = 'Invalid Credentials. Please try again.'
             else:
                 #save instance of user and id of user
                 session['logged_in_user'] = request.form['username']
                 session['user_id'] = loginval.first().id
-                
+                session['user_cart'] = []
                 return redirect(url_for('index'))
 
         #if signup button is clicked on HTML side name == button value(Sign Up)
@@ -91,11 +107,30 @@ def login():
 def logout():
     session['logged_in_user'] = None
     session['user_id'] = None
+    session['user_cart'] = None
     return redirect(url_for('index'))    
 
 #render signup page
 @app.route("/signingup",methods=['GET', 'POST'])                  
 def signup_page():
+
+    if request.method == 'POST':
+       
+        #if login button is clicked on HTML side name == button value(Login)
+        if request.form['submit_but'] == "Complete Sign Up":
+            id = db.session.query(Users.id).count() + 1
+            print(id)
+            user_signup = Users(id,request.form['firstname'], request.form['lastname'], request.form["street_num"],\
+            request.form["street_name"], request.form["postalcode"],\
+            request.form["country"], request.form["province"],\
+            request.form["phone_number"], request.form["email"],\
+            request.form["username"], request.form["password"])
+            
+            #print(request.form["firstname"])
+            db.session.add(user_signup)
+            db.session.commit()
+            return redirect(url_for('login'))
+
     return render_template('signup.html')     
 
 #render Cart
@@ -104,14 +139,20 @@ def cart():
     #store directory of folder
     working_dir = os.path.dirname(os.path.abspath( __file__ ))
     main_dir = os.path.dirname(working_dir)
-    
+    showbooks = []
     #Check is user is logged in
     if session.get('logged_in_user') is None:
         username = "guest"
     else:
         username = session.get('logged_in_user')
+        disp_cart = session['user_cart']
+        
+        for i in range(len(disp_cart)):
+            showbooks.append(db.session.query(Books.title, Books.isbn, Books.price).filter_by(isbn = disp_cart[i]).first())
+       
+        len_books = len(showbooks)
 
-    return render_template('cart.html', username=username)
+    return render_template('cart.html', username=username, showbooks=showbooks, len_books=len_books)
 
 
 
--- models.py
+++ models.py
@@ -10,18 +10,14 @@ class Books(db.Model):
 
     __tablename__ = "books"
     book_id = db.Column(db.Integer(), primary_key=True)
-    publisher = db.Column(db.String())
-    author = db.Column(db.String())
+    isbn = db.Column(db.String(), primary_key=True)
     title = db.Column(db.String())
-    isbn = db.Column(db.String())
     publication_date = db.Column(db.String())
     price = db.Column(db.String())
     book_stock = db.Column(db.Integer())
 
-    def __init__(self, book_id, author, publisher, title, isbn, genre, publication_date, price,book_stock):
+    def __init__(self, book_id, title, isbn, genre, publication_date, price,book_stock):
         self.book_id = book_id
-        self.author = author
-        self.publisher = publisher
         self.title = title
         self.isbn = isbn
         self.publication_date = publication_date
@@ -29,7 +25,7 @@ class Books(db.Model):
         self.book_stock = book_stock
 
     def __repr__(self):
-        return '<title %r>' % (self.title, self.author, self.publisher, self.isbn, self.publication_date)
+        return '<title %r>' % (self.title, self.isbn, self.publication_date)
         
 
 class Users(db.Model,UserMixin):
@@ -39,6 +35,7 @@ class Users(db.Model,UserMixin):
     firstname = db.Column(db.String())
     lastname = db.Column(db.String())
     street_num = db.Column(db.String())
+    street_name = db.Column(db.String())
     postalcode = db.Column(db.String())
     country = db.Column(db.String())
     province = db.Column(db.String())
@@ -47,11 +44,12 @@ class Users(db.Model,UserMixin):
     username = db.Column(db.String())
     password = db.Column(db.String())
 
-    def __init__(self, id, firstname, lastname, street_num, postalcode, country, province, phone_number, email, username, password):
-        self.id = cust_id
+    def __init__(self, id, firstname, lastname, street_num,street_name, postalcode, country, province, phone_number, email, username, password):
+        self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.street_num = street_num
+        self.street_name = street_name
         self.postalcode = postalcode
         self.country = country
         self.province = province
@@ -63,6 +61,59 @@ class Users(db.Model,UserMixin):
     def __repr__(self):
         return self.id
 
+class Published(db.Model):
+    __tablename__ = "published"
+    p_id = db.Column(db.Integer(), primary_key=True)
+    book_id = db.Column(db.Integer(), primary_key=True)
+    isbn = db.Column(db.String(), primary_key=True)
+
+class Publisher(db.Model):
+
+    __tablename__ = "publisher"
+    p_id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(), unique=True)
+    firstname = db.Column(db.String())
+    lastname = db.Column(db.String())
+    street_num = db.Column(db.String())
+    postalcode = db.Column(db.String())
+    country = db.Column(db.String())
+    province = db.Column(db.String())
+    phone_number = db.Column(db.String())
+    province = db.Column(db.String())
+    publisher_name = db.Column(db.String())
+    bank_acc = db.Column(db.String())
+
+    def __init__(self, id, email, street_num, postalcode, country, province, phone_number,publisher_name, bank_acc):
+        self.p_id = p_id
+        self.email = email
+        self.street_num = street_num
+        self.postalcode = postalcode
+        self.country = country
+        self.province = province
+        self.phone_number = phone_number
+        self.publisher_name = publisher_name
+        self.bank_acc = bank_acc
+
+    def __repr__(self):
+        return self.id
+
+
+
+class Wrote(db.Model):
+
+    __tablename__ = "wrote"
+    a_id = db.Column(db.Integer(), primary_key=True)
+    book_id = db.Column(db.String(), primary_key=True)
+    isbn = db.Column(db.String(), primary_key=True)
+
+
+
+class Author(db.Model):
+
+    __tablename__ = "author"
+    a_id = db.Column(db.Integer(), primary_key=True)
+    firstname = db.Column(db.String())
+    lastname = db.Column(db.String())
 
 
 
